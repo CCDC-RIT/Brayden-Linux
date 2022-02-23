210f45279bf2e20c477acf0e5af6bc45631709bc
P4ral1ax <braydenjw00@gmail.com>
2022-02-22 20:39:57 -0500

    updated funny_finder and other things

--- one-liners.sh
+++ one-liners.sh
@@ -11,3 +11,13 @@ find / -xdev -mmin -10 -ls 2> /dev/null
 
 # Check for backdoored binaries
 for i in $(dpkg -l | awk '{print $2}'); do sudo dpkg --verify $i; done
+
+# Find Potential Rootkit Persistance
+find /etc/init.d -exec cat {} + | grep -E 'insmod|modprobe' 2> /dev/null
+find /etc/systemd/system -exec cat {} + | grep -E 'insmod|modprobe' 2> /dev/null
+
+
+# TODO
+AuditD - AUSomthing
+Syslong
+https://restic.net/
